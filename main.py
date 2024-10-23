c7750b3ba9eaad99d3741a48e7f371c2141fc745
floppa <jbarek2027@gmail.com>
2024-10-23 10:43:47 -0700

    Finished

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ import turtle
 
 # YOUR CODE HERE
 
+for _ in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.forward(50)
+turtle.left(45)
+for _ in range(4):
+    turtle.forward(71)
+    turtle.left(90)
+
+
+
+
 turtle.exitonclick()
